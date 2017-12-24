482e40794f8fb25bd9706077729b98bcb6bdbc76
James Patmon <james.patmon@outlook.com>
2017-12-23 23:40:03 -0500

    Update to include sum of each vowel

--- count_vowels/count_vowels.js
+++ count_vowels/count_vowels.js
@@ -1,28 +1,68 @@
-function isVowel(letter) {
+function vowelSearch(letter) {
     var vowels = 'aeiou', max = 4, min = 0;
     while (min <= max) {
         var mid = Math.ceil((max + min) / 2);
         if (letter == vowels[mid])
-            return true;
+            return vowels[mid];
         if (letter < vowels[mid])
             max = mid - 1;
         else
             min = mid + 1;
     }
-    return false;
+    return null;
 }
 
 function countVowels(word) {
-    var vowelSum = 0;
-    for (let i = 0; i < word.length; i++) {
-        if (isVowel(word[i]))
-            vowelSum++;
+    var vowelSum = a = e = i = o = u = 0;
+    for (let j = 0; j < word.length; j++) {
+        switch (vowelSearch(word[j])) {
+            case 'a':
+                a++;
+                break;
+            case 'e':
+                e++;
+                break;
+            case 'i':
+                i++;
+                break;
+            case 'o':
+                o++;
+                break;
+            case 'u':
+                u++;
+                break;
+            default:
+                break;
+        }
     }
 
-    return vowelSum;
+    var vowel = function (letter) {
+        if (letter == 'a')
+            return a;
+        if (letter == 'e')
+            return e;
+        if (letter == 'i')
+            return i;
+        if (letter == 'o')
+            return o;
+        if (letter == 'u')
+            return u;
+        else
+            return -1;
+    }
+    return vowel;
 }
 
 var sum = countVowels('antidisestablishmentarianism');
-console.log('There are ' + sum + ' vowels.');
+var a = sum('a'), e = sum('e'), i = sum('i'), o = sum('o'), u = sum('u');
+var total = a + e + i + o + u;
+console.log('There are ' + a + ' As.\nThere are ' + e + ' Es.\nThere are ' + i + ' Is.\nThere are ' + o + ' Os.\nThere are ' + u + ' Us.\nTotal vowels: ' + total);
 
-// -> There are 11 vowels.
+/*
+    There are 4 As.
+    There are 2 Es.
+    There are 5 Is.
+    There are 0 Os.
+    There are 0 Us.
+    Total vowels: 11
+*/
