36c8344785c2864a7d410772be6413430880b1fe
James Patmon <james.patmon@outlook.com>
2017-12-23 19:42:40 -0500

    Add Pig Latin solution

---
+++ pig_latin/pig_latin.js
@@ -0,0 +1,32 @@
+function isVowel(letter) {
+    var vowels = 'aeiou';
+    for (let i = 0; i < vowels.length; i++) {
+        if (letter == vowels[i]) return true;
+    }
+    return false;
+}
+
+function translate(word) {
+    var prefix = '';
+    var postfix = '';
+
+    for (var i = 0; i < word.length; i++) {
+        let isAVowel = isVowel(word[i]);
+        if (!isAVowel) {
+            postfix += word[i];
+        } else {
+            break;
+        }
+    }
+
+    for (let j = i; j < word.length; j++) {
+        prefix += word[j];
+    }
+
+    var translation = prefix + postfix + 'ay';
+    return translation;
+}
+
+var consonant = translate('thanks');
+var vowel = translate('omelet');
+console.log(consonant + '\n' + vowel);
